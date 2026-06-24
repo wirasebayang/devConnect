56ef311c155b1dc257d9d428369613646525d587
M Rusdiana <muhamadrusdiana452@gmail.com>
2026-06-24 17:00:04 +0700

    prog2

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let loginBtn = document.querySelector('.login')
-let username = localStorage.getItem('username');
+let username = JSON.parse(localStorage.getItem('username'))
 let viewLog = document.querySelector('.login-btn')
 let profile = document.querySelector('.name')
+let profiles = document.querySelector('.name-i')
 let menu = document.querySelector('.menu')
 let menus = document.querySelector('.names')
 let nav = document.querySelector('.navbar')
@@ -12,37 +13,38 @@ let afterLog = document.querySelector('.afterLogin')
 let logoutBtn = document.querySelector('.logout')
 let formView = document.querySelector('.formLogin')
 let body = document.getElementsByTagName('body')
+let inputUser = document.getElementById('username')
+let inputPassword = document.getElementById('password')
+let submit = document.querySelector('.login-Btn')
 
 if(username){
     viewLog.style.display = "none";
-    profile.style.color = " var(--cyan)"
-    profile.style.fontWeight = "600"
-    profile.innerText = username[0].toUpperCase() + username[1].toUpperCase()
+    profile.innerText = username.username[0].toUpperCase() + username.username[1].toUpperCase()
+    profiles.innerText = username.username[0].toUpperCase() + username.username[1].toUpperCase()
     profile.classList.toggle('title')
-    menus.innerText = username
+    profiles.classList.toggle('title')
+    menus.innerText = username.username
     nav.style.display = "none"
     logo.style.flex = "2"
     afterLog.classList.toggle('views')
-    // formView.classList.toggle('formView')
 }
 
-loginBtn.addEventListener('click', () => {  
 
-    // formView.classList.toggle('formView')
+if(submit) {
+    submit.addEventListener('click', (e) => {
+
+        e.preventDefault()
     
+        if(!inputUser.value || !inputPassword.value){
+            alert('Data Invalid')
+        } else {
+            localStorage.setItem('username', JSON.stringify({username: inputUser.value, password: inputPassword.value}))
+            
+            window.location.replace("index.html")
+        }
+    })
+}
 
-    let name = prompt('Masukan nama mu');
-    
-    if(!name){
-        alert('Data Invalid')
-        e
-    } else {
-
-        JSON.stringify(localStorage.setItem('username', name))
-    }
-
-    // event.preventDefault()
-})
 
 profile.addEventListener('click', () => {
     menu.classList.add('view')
